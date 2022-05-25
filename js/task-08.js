d129ba09cbe8daff0313600d2db9899734e759e2
Vladyslav Avidzba <wasssd1@gmail.com>
2022-05-25 16:44:56 +0300

    task8-9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-
+document
+  .querySelector("form.login-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (!email.value || !password.value) {
+      alert("All fields should be input");
+      return;
+    }
+    console.log({
+      [email.name]: email.value,
+      [password.name]: password.value,
+    });
+  });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanRef = document.querySelector("span.color");
+document.querySelector("button.change-color").addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanRef.textContent = color;
+});
