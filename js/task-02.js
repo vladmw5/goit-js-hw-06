d8e52464ac63efcef74551de31861619d769142b
Vladyslav Avidzba <wasssd1@gmail.com>
2022-05-25 15:26:54 +0300

    task2-3 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+document.querySelector("#ingredients").append(
+  ...ingredients.map((ingredient) => {
+    const listElemRef = document.createElement("li");
+    listElemRef.classList.add("item");
+    listElemRef.textContent = ingredient;
+    return listElemRef;
+  })
+);
