f55a53cee6fbf8289cc9bedf35bcb19b7bbf7bb1
Vladyslav Avidzba <wasssd1@gmail.com>
2022-05-26 21:17:45 +0300

    task6-fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,15 @@
+function addRemoveClass(toAdd, toRemove) {
+  this.classList.add(toAdd);
+  this.classList.remove(toRemove);
+}
+
 document
   .querySelector("#validation-input")
   .addEventListener("blur", (event) => {
     const input = event.currentTarget;
     if (input.value.length === Number(input.dataset.length)) {
-      input.classList.add("valid");
-      input.classList.remove("invalid");
+      addRemoveClass.call(input, "valid", "invalid");
     } else {
-      input.classList.remove("valid");
-      input.classList.add("invalid");
+      addRemoveClass.call(input, "invalid", "valid");
     }
   });
